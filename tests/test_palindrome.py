c50f9b9af2a19e717a07cc1b07c1f6a38dce66eb
luks19 <l.valdemoros@alumno.um.edu.ar>
2025-04-15 18:27:43 +0000

    agregue los test para frases palindromes

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -8,7 +8,13 @@ class TestPalindrome(unittest.TestCase):
         self.assertTrue(is_palindrome("salas"))
         self.assertFalse(is_palindrome("hello"))
         self.assertFalse(is_palindrome("hamburguesa"))
+    def test_frases_palindormes(self):
+        self.assertTrue(is_palindrome("Anita lava la tina"))
+        self.assertTrue(is_palindrome("Amo la pacífica paloma"))
+        self.assertTrue(is_palindrome("La ruta natural"))
         
+
+
 if __name__ == '__main__':
      unittest.main()
 
