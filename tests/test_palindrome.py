152839a27b67b60564c6cddf41e2c08c5cc405f1
luks19 <l.valdemoros@alumno.um.edu.ar>
2025-04-15 16:11:28 +0000

    agregamos frases edge

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -8,19 +8,23 @@ class TestPalindrome(unittest.TestCase):
         self.assertTrue(is_palindrome("salas"))
         self.assertFalse(is_palindrome("hello"))
         self.assertFalse(is_palindrome("hamburguesa"))
+
     def test_frases_palindormes(self):
         self.assertTrue(is_palindrome("Anita lava la tina"))
         self.assertTrue(is_palindrome("Amo la pacífica paloma"))
         self.assertTrue(is_palindrome("La ruta natural"))
+
     def test_frases_no_palindromas(self):
         self.assertFalse(is_palindrome("El cielo es azul"))
         self.assertFalse(is_palindrome("La luna es blanca"))
         self.assertFalse(is_palindrome("El sol brilla"))
-    def test_frases_palindormes(self):
-        self.assertTrue(is_palindrome("Anita lava la tina"))
-        self.assertTrue(is_palindrome("Amo la pacífica paloma"))
-        self.assertTrue(is_palindrome("La ruta natural"))
-        
+
+    def test_casos_edge(self):
+        self.assertTrue(is_palindrome(""))
+        self.assertTrue(is_palindrome("a"))
+        self.assertTrue(is_palindrome("como 'te'va"))
+        self.assertTrue(is_palindrome("HoLa"))
+        self.assertTrue(is_palindrome("123431"))
 
 if __name__ == '__main__':
      unittest.main()
